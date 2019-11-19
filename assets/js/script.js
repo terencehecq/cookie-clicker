a8ac5224ba654c422adb283c76b7fa6ba40e1541
Vincent De Gheyndt <vincent.degheyndt@gmail.com>
2019-11-19 15:16:01 +0100

    maj2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let multiplicateur= document.getElementById("multiplicateur");
 let compteur= 1;//valeur par click
 let score= 0;//argent
 let multipli = 1;
-let coutMultiplicateur= 50
+let coutMultiplicateur= 10
 
 bouton.addEventListener("click", () => {
     score+= compteur * multipli;
@@ -13,10 +13,11 @@ bouton.addEventListener("click", () => {
 
 });
 
-multiplicateur.addEventListener("click", () => {
+multiplicateur.addEventListener("click", (event) => {
     if (score>=coutMultiplicateur){
     score= score-coutMultiplicateur;
     multipli= multipli*2;
+    event.target.textContent ="multiplicateur: " + multipli
+    
     }
-
 });
