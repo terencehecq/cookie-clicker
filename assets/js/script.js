5a1f5f4c9109d20595c4353416e310314869ad53
Térence <terencehecq@gmail.com>
2019-11-19 16:24:40 +0100

    auto clicker màj

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 let bouton = document.getElementById("click");
 let affichage = document.getElementById("affichage");
 let multiplicateur = document.getElementById("multiplicateur");
+let autoClicker = document.getElementById("autoClicker");
 let compteur = 1; //valeur par click
 let score = 0; //argent
 let multipli = 1;
 let nbMultiplicateur = 0;
 let coutMultiplicateur = 10;
+let autoTime = 10000;
+let nbAutoClick = 0;
+let coutAutoClick = 50;
 
 bouton.addEventListener("click", () => {
   score += compteur * multipli;
@@ -24,17 +28,26 @@ multiplicateur.addEventListener("click", event => {
   }
 });
 
-autoClicker.addEventListener("click", function autoClick(){
-  let interval = setInterval(() => {
-      score+= compteur * multipli;
-      affichage.textContent= score;
-  }, autoTime);
-  autoClicker.removeEventListener("click",autoClick)
-  autoClicker.addEventListener("click", function lowerAutoTime(){
-      autoTime = autoTime*0.9 ; 
-      console.log(autoTime);
-      clearInterval(interval);
-      autoClick();
-      autoClicker.removeEventListener("click", lowerAutoTime)
-  })
+autoClicker.addEventListener("click", function autoClick(e){
+  if (score >= coutAutoClick) {
+    score = score - coutAutoClick;
+    affichage.textContent = score;
+    nbAutoClick += 1;
+    coutAutoClick *= nbAutoClick;
+    autoClicker.textContent = `Auto-click++ / price : ${coutAutoClick}`
+
+    let interval = setInterval(() => {
+        score+= compteur * multipli;
+        affichage.textContent= score;
+    }, autoTime);
+
+    autoClicker.removeEventListener("click",autoClick)
+    autoClicker.addEventListener("click", function lowerAutoTime(){
+        autoTime = autoTime*0.9 ; 
+        console.log(autoTime);
+        clearInterval(interval);
+        autoClick();
+        autoClicker.removeEventListener("click", lowerAutoTime)
+    })
+  }
 });
